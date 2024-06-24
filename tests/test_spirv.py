8517115bf72aef56e14c8625d0919ac498ba610b
Victor van den Elzen <victor.de@gmail.com>
2024-06-24 17:50:50 +0200

    add fotgotten file test_spirv

---
+++ tests/test_spirv.py
@@ -0,0 +1,62 @@
+import vutur.spirv_instructions as si
+import vutur.spirv_composite as sc
+
+
+def test_empty_module() -> None:
+    bb = sc.SpirvBlock(
+        label=si.OpLabel(),
+        instructions=[
+            si.OpReturn(),
+        ],
+    )
+
+    void = si.OpTypeVoid()
+
+    ftype = si.OpTypeFunction(void)
+
+    f = sc.SpirvFunction(
+        function=si.OpFunction(void, si.FunctionControl(0), ftype),
+        parameters=[],
+        blocks=[bb],
+    )
+
+    module = sc.SpirvModule(
+        func_decls=[],
+        func_defs=[f],
+    )
+
+    b = module.serialize()
+
+    print(sc.disasm(b))
+
+    sc.validate(b)
+
+
+def test_square_module() -> None:
+    bb = sc.SpirvBlock(
+        label=si.OpLabel(),
+        instructions=[
+            si.OpReturn(),
+        ],
+    )
+
+    void = si.OpTypeVoid()
+
+    ftype = si.OpTypeFunction(void)
+
+    f = sc.SpirvFunction(
+        function=si.OpFunction(void, si.FunctionControl(0), ftype),
+        parameters=[],
+        blocks=[bb],
+    )
+
+    module = sc.SpirvModule(
+        func_decls=[],
+        func_defs=[f],
+    )
+
+    b = module.serialize()
+
+    print(sc.disasm(b))
+
+    sc.validate(b)
